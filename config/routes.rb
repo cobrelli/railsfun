df2c4a8e1f3fd1b6841a683e1c7364e49627ef39
Tomi Virtanen <cobrelli@gmail.com>
2013-10-28 14:18:24 +0200

    linkkejä ja sivuja

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Railsfun::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  match '/about' => 'pages#about', via: [:get, :post]
+  match '/contact' => 'pages#contact', via: [:get, :post]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
